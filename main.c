c43c6d60dc2fff46202a7bb006aa4dcf1e0ac7cc
Dat Huynh <huynhtiendat22032007@gmail.com>
2026-03-01 15:30:23 +0700

    Hoàn thành chức năng chuyển tiền

--- main.c
+++ main.c
@@ -208,7 +208,7 @@ void guitien(node root, node myAccount)
         node targetAccount = findNode(root, account);
         if (targetAccount != NULL)
         {
-            int sotiengui = chonsotiengui(targetAccount, myAccount);
+            long long sotiengui = chonsotiengui(targetAccount, myAccount);
 
             if (sotiengui == 0) return;
 
@@ -241,14 +241,14 @@ void guitien(node root, node myAccount)
                 }
 
                 //Kiểm tra số lần giao dịch phiên
-                if (admin_index > GIAODICHPHIEN)
+                if (admin_index >= GIAODICHPHIEN)
                 {
                     printf("*** QUÁ SỐ LẦN GIAO DỊCH PHIÊN ***\n\n");
                     return;
                 }
 
                 //Kiểm tra số lần giao dịch trong ngày
-                if (myAccount-> Data->TransactionCount  > MAX_DAILY_TRANSACTION)
+                if (myAccount-> Data->TransactionCount  >= MAX_DAILY_TRANSACTION)
                 {
                     printf("*** QUÁ SỐ LẦN GIAO DỊCH TRONG NGÀY ***\n\n");
                     return;
@@ -333,18 +333,220 @@ void guitien(node root, node myAccount)
     if (AccountEntryCount == 0) printf("*** QUÁ SỐ LẦN NHẬP TÀI KHOẢN ***\n\n");
 }
 
+//Hàm chọn số tiền chuyển đi
+long long  chonsotienchuyen(node target, node myAccount)
+{
+    //Hạn mức tối đa
+    long long hanmuctoida = 100000000;
+    
+    
+    long long arr[8] = {0 ,100000, 200000, 500000, 1000000, 2000000, 3000000, 0};
+    
+    //In menu số tiền được chọn
+    int i = 0;
+    for (i = 0; i < 8; i++)
+    {
+        if (i == 0) printf("%d. Quay về menu        ", i);
+        else if (i == 7) printf("%d. Số khác\n\n", i);
+        else
+        {
+            printf("%d. %lld VND       ",i,arr[i]);
+            if (i == 2 || i == 5) printf("\n\n");
+        }
+    }
+
+    printf("Chọn số tiền bạn muốn chuyển: ");
+    scanf("%d", &i);
+    int clear = clear_buffer();
+    if (i == 0) return 0;
+    else if (i == 7)
+    {
+        printf("Hạn mức tối đa là : %lld VND\n\n", hanmuctoida);
+
+        int tempEntry = 3;
+        long long temp;
+        do
+        {
+            if (tempEntry == 0)
+            {
+                printf("*** QUÁ SÔ LẦN NHẬP ***\n\n");
+                return 0;
+            }
+
+            printf("Vui lòng nhập bội số của 50.000 VND (Bạn có %d lần nhập): ", tempEntry);
+            scanf(" %lld", &temp);
+            int clear = clear_buffer();
+            tempEntry -= 1;
+        } while (temp % 50000 != 0 || temp > hanmuctoida || temp <= 0);
+        return temp;
+    }
+    else if (i < 0 || i > 7) return 0;
+    else return arr[i];
+}
+
+//Chức năng chuyển tiền
+void chuyentien(node root, node myAccount)
+{
+    char account[MAX_ACC_LEN];
+    char yesno = 'Y';
+    int AccountEntryCount = 3 ;
+
+    do 
+    {
+        printf("Nhập số tài khoản bạn muốn gửi tiền (Bạn còn %d lần nhập): ", AccountEntryCount);
+        scanf(" %20s", account);
+        int clear = clear_buffer();
+        AccountEntryCount -= 1;
+
+        //Kiểm tra trùng lặp tài khoản nguồn
+        if (strcmp(account, myAccount ->Data ->AccountNumber) == 0)
+        {
+            printf("*** TRÙNG LẶP TÀI KHOÀN NGUỒN ***\n\n");
+            printf("Bạn muốn nhập lại Số tài khoản không? [Y/N]: ");
+            scanf(" %c", &yesno);
+            clear = clear_buffer();
+        }
+        else
+        {
+            node targetAccount = findNode(root, account);
+            if (targetAccount != NULL)
+            {
+                long long sotienchuyen = chonsotienchuyen(targetAccount, myAccount);
+
+                if (sotienchuyen == 0) return;
+
+
+                else if  (sotienchuyen != 0)
+                {
+                    char message[81]; //Trừ hao 50 kí tự ghi +VND
+                    int message_Entry_Count = 3;
+                    int clear = 0;
+
+
+                    //Nhập lời nhắn nạp tiền
+                    do
+                    {
+                        if (clear == 1) printf("*** QUÁ 80 KÍ TỰ ***\n\n");
+
+                        printf("Nhập lời nhắn với 80 kí tự (Bạn còn %d lần nhập): ", message_Entry_Count);
+                        message_Entry_Count -= 1;
+
+                        //Vì scanf sẽ dừng nếu gặp dấu cách khiến buffer hiểu lầm rằng dư kí tự --> cho phép \n
+                        scanf(" %80[^\n]", message); //Trừ hao 50 kí tự ghi +VND
+                        clear = clear_buffer();
+                    } while (clear == 1 && message_Entry_Count > 0);
+
+
+                    //Khi thoát ra khỏi vòng lặp mà clear == 1 thì nghĩa là message_Entry_Count == 0
+                    if (clear == 1)
+                    {
+                        printf("*** QUÁ SỐ LẦN NHẬP LỜI NHẮN ***\n\n");
+                        return;
+                    }
+
+                    //Kiểm tra số tiền tài khoản nguồn
+                    if (myAccount ->Data ->Balance < sotienchuyen)
+                    {
+                        printf("*** SỐ DƯ TÀI KHOẢN KHÔNG ĐỦ ĐỂ THỰC HIỆN GIAO DỊCH ***\n\n");
+                        return;
+                    }
+
+                    //Kiểm tra số lần giao dịch phiên
+                    if (admin_index >= GIAODICHPHIEN)
+                    {
+                        printf("*** QUÁ SỐ LẦN GIAO DỊCH PHIÊN ***\n\n");
+                        return;
+                    }
+
+                    //Kiểm tra số lần giao dịch trong ngày
+                    if (myAccount-> Data->TransactionCount  >= MAX_DAILY_TRANSACTION)
+                    {
+                        printf("*** QUÁ SỐ LẦN GIAO DỊCH TRONG NGÀY ***\n\n");
+                        return;
+                    }
+                    
+
+                    //Kiểm tra số lần giao dịch đối với lịch sử My Account
+                    if (myAccount ->Data ->TransactionCount >= MAX_TRASACTION_LEN)
+                    {
+                        printf(">>>\n\nGIAO DỊCH THẤT BẠI: VƯỢT QUÁ SỐ LẦN GIAO DỊCH TỐI ĐA TRONG LỊCH SỰ GIAO DỊCH ĐỐI VỚI TÀI KHOẢN CỦA BẠN\n\n");
+                        return;
+                    }
+
+                    //Kiểm tra số lần giao dịch đối với lịch sử TargetAccount
+                    if (targetAccount ->Data ->TransactionCount >= MAX_TRASACTION_LEN)
+                    {
+                        printf(">>>\n\nGIAO DỊCH THẤT BẠI: VƯỢT QUÁ SỐ LẦN GIAO DỊCH TỐI ĐA TRONG LỊCH SỰ GIAO DỊCH ĐỐI VỚI TÀI KHOẢN GỬI ĐẾN\n\n");
+                        return;
+                    }
+
+
+                    int targetAccount_current_index = targetAccount -> Data -> TransactionCount;
+                    int myAccount_current_index = myAccount -> Data -> TransactionCount;
+
+                    //Trừ tiền vào myAccount
+                    myAccount ->Data -> Balance -= sotienchuyen;
+
+                    //Cộng tiền vào targetAccount
+                    targetAccount-> Data-> Balance += sotienchuyen;
+
+                    //Thêm lời nhắn vào thông báo
+                    sprintf(myAccount ->Data ->TransactionHistory[myAccount_current_index], "Chuyển %lld VND đến tài khoản %s", sotienchuyen, targetAccount->Data->AccountNumber);
+                    sprintf(targetAccount ->Data ->TransactionHistory[targetAccount_current_index], "+%lld VND|ND: %s từ %s",sotienchuyen, message, myAccount ->Data ->AccountNumber); //Tối đa là 130 kí tự
+
+                    //Thêm vào danh sách giao dịch admin
+                    strcpy(TransactionList[admin_index], myAccount ->Data ->TransactionHistory[myAccount_current_index]);
+                    admin_index +=1;
+
+                    //Cộng số lần giao dịch
+                    myAccount-> Data-> TransactionCount += 1;
+                    targetAccount-> Data-> TransactionCount += 1;
+
+                    printf("\n\n>>> GIAO DỊCH THÀNH CÔNG\n\n");
+                    return;
+                }
+            }
+            else
+        {
+            printf("\n\n*** Không tìm thấy Số tài khoản ***\n\n");
+            printf("Bạn muốn nhập lại Số tài khoản không? [Y/N]: ");
+            scanf(" %c", &yesno);
+            int clear = clear_buffer();
+        }
+        }
+    } while (yesno == 'Y' && AccountEntryCount > 0);
+    if (AccountEntryCount == 0) printf("*** QUÁ SỐ LẦN NHẬP TÀI KHOẢN ***\n\n");
+}
+
+
 //Menu
 void menu(node root, node myAccount)
 {
-    int option = 0;
-    printf("1. Gửi tiền\n\n");
-    scanf(" %d" , &option);
+    //Mảng con trỏ, Nhược điểm: Không sửa được, Ưu điểm: Cấp phát đúng bộ nhớ cần thiết
+    char *option[] = {"0. Hủy giao dịch","1. Gửi tiền", "2. Chuyển tiền"};
+
+    int optionCount = sizeof(option)/ sizeof(option[0]);
+
+    int i;
+    for (i = 0; i< optionCount; i+=1)
+    {
+        printf("%s\n\n",option[i]);
+    }
+
+    printf("Nhập sự lựa chọn của bạn: ");
+    scanf(" %d" , &i);
     int clear = clear_buffer();
-    if (option == 1) 
+    if (i == 1) 
     {
         guitien(root, myAccount);
         return;
     }
+    else if (i == 2)
+    {
+        chuyentien(root, myAccount);
+        return;
+    }
+    else return;
 }
 
 //Hàm đăng nhập với vai trò khách
@@ -381,6 +583,8 @@ void GuestLogin(node root)
             //Kiểm tra mã pin
             if (strcmp(temp ->Data -> Pin, mapin) ==0) 
             {
+
+                //Vào menu
                 printf("Vào menu\n\n");
                 menu(root, temp);
                 return;
