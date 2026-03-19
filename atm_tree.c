fa33b5f5bde1b62862c251e71cd175cd15fc1306
leviethuy123 <102250071@sv1.dut.udn.vn>
2026-03-19 08:43:17 +0700

    Create atm_tree.c

---
+++ atm_tree.c
@@ -0,0 +1,57 @@
+#include "atm_types.h"
+Account append_account(char *AN, char *PI, unsigned long long BL)
+{
+    Account temp = (Account)malloc(sizeof(struct Account));
+
+    //dùng strcpy để copy xâu 2 qua xâu 1
+    strcpy(temp -> AccountNumber,AN);
+    strcpy(temp -> Pin, PI);
+    temp -> Balance = BL;
+    temp ->TransactionCount = 0;
+    return temp;
+}
+//Hàm thêm nhánh vào Tree
+node CreateNode (Account d)
+{
+    // hàm malloc để cấp phát bộ nhớ heap, (node) để ép kểu, sizeof(struct TreeNode) kích thước xin cấp
+    node temp = (node)malloc(sizeof(struct TreeNode));
+    temp -> Data = d;
+    temp -> left = NULL;
+    temp -> right = NULL; 
+    return temp;
+}
+//Hàm tạo Tree từ nhánh
+node CreateTree (node root, node a)
+{
+    //Điểm dừng của quá trình đệ quy
+    if (root ==  NULL) 
+    {
+        return a;
+    }
+
+    //dùng hàm strcmp để so sánh 2 xâu account number, str1 > str2 thì compare =1
+    int compare = strcmp(a-> Data->AccountNumber, root -> Data -> AccountNumber);
+
+
+    if (compare ==1) root -> right = CreateTree((*root).right, a);
+    
+    else if (compare == -1) root -> left = CreateTree(root -> left,a);
+    
+    else printf("Lỗi trùng lặp số tài khoản %s\n", a-> Data ->AccountNumber);
+
+    //trả về root ban đầu
+    return root;
+}
+//Hàm tìm kiếm Node trong Tree
+node findNode(node root, char *target)
+{
+    //Nếu root = NULL thì nghĩa là đã tìm hết Tree nhưng không tìm thấy
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    int compare = strcmp(target, root->Data->AccountNumber);
+    if (compare == 0) return root;
+    else if (compare == 1) return findNode(root -> right, target);
+    else return findNode(root -> left, target);
+}
